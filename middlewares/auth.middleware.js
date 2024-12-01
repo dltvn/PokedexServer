d09f8a53e88a89fe0f81fe2aaf2fbfd5f30ce6d0
Mina <bellella13@gmail.com>
2024-11-30 21:21:55 -0500

    wipwip

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,13 +1,40 @@
 import { ObjectId } from "mongodb";
+import jwt from "jsonwebtoken";
 
 export function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated() && req.user) {
-    const userId = req.user._id;
-    if (!ObjectId.isValid(userId)) {
-      return res.status(400).json({ error: "Invalid user ID" });
-    }
-    req.userId = userId;
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+export function authenticate(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
     return next();
   }
-  res.status(401).json({ error: "Unauthorized: User is not logged in." });
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    req.userId = decoded.id;
+  } catch (err) {
+  } finally {
+    next();
+  }
 }
